2ffb1b5828f1933a4ca7a66f33b950e78736a787
vikashgupta16 <vikash9c35@gmail.com>
2024-05-30 10:30:14 +0530

    lock

---
+++ js/lock.js
@@ -0,0 +1,12 @@
+<script language="JavaScript">
+    <! --hide
+    var password;
+    var pass1="AMIT INSTITUTE";
+    password=prompt('Enter Password To View Page',' ');
+    if (password==pass1)
+    alert('Correct password, click OK to enter.');
+    else{
+        window.location="https://vikashgupta16.github.io/aims/"
+    }
+    //--> 
+</script>
