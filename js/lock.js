7b54bc2dfff29a0b8dc62a3aaebec3479114db65
vikashgupta16 <vikash9c35@gmail.com>
2024-06-02 10:15:30 +0530

    open

--- js/lock.js
+++ js/lock.js
@@ -5,9 +5,9 @@ function unlockPage() {
     if (passwordInput === correctPassword) {
         // Redirect to the unlocked page
         window.location.href = "physics.html";
-        // window.location.href = "chemistry.html";
-        // window.location.href = "maths.html";
-        // window.location.href = "biology.html"; // Change "unlocked.html" to the path of your unlocked HTML page
+        window.location.href = "chemistry.html";
+        window.location.href = "maths.html";
+        window.location.href = "biology.html"; // Change "unlocked.html" to the path of your unlocked HTML page
     } else {
         alert("Incorrect password. Please try again.");
     }
